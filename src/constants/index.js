8ee435ced72caaccf95ce7795da7074d387890b0
Muhammad Nadeem <150074779+nadeemkhn@users.noreply.github.com>
2024-12-10 14:54:40 +0500

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -12,17 +12,20 @@ export const Projects=[
         ,
         Technologies :["ReactJs  |","Tailwindcss"]
     },
-    {
-        Title:"Discover KhyberPashtunkhwa",
-        Description:" Created a responsive front-end website with React.js and Bootstrap to showcase various areas of KPK",
-        Technologies:["Reactjs |","Bootstrap"]
 
-    }
+       {
+        Title:"Medicine Pharmacy",
+        Description:"Developed a custom Odoo module to streamline operations for a medicine distribution company.
+                     Features include managing medicine inventory, tracking suppliers, monitoring stock levels, and
+                     generating detailed sales and distribution report "
+        ,
+        Technologies :["Python  |","XML"]
+    },
 
 ];
 
 export const contact = {
-    address: "Ali Town Lahore ",
+    address: "Johar Town Lahore ",
     phoneNo: "+92 3059290390 ",
     email: "nadeemwazir0123@gmail.com",
   };

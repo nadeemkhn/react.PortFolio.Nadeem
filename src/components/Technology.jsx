3b3b3aaf88e47821ebcf2ed26591b2c78e51a182
Muhammad Nadeem <150074779+nadeemkhn@users.noreply.github.com>
2025-03-17 14:58:40 +0500

    Update Technology.jsx

--- src/components/Technology.jsx
+++ src/components/Technology.jsx
@@ -8,6 +8,12 @@ const Technology = () => {
     <div className="border-b border-neutral-800 pb-24">
       <h1 className="my-20 text-center text-4xl">Technologies</h1>
       <div className="flex flex-wrap items-center justify-center gap-6">
+          <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            <SiOdoo className="text-7xl text-cyan-400" />
+        </div>
+        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            <DiPostgresql className="text-7xl text-cyan-400" />
+        </div>
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
             <FaHtml5 className="text-7xl text-cyan-400" />
         </div>
